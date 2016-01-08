bd8dd2e8e946fb7345b7cfddafff6acb6e29aac3
Adrian Ottiker <adrian@ottiker.com>
2016-01-08 14:14:43 +0000

    export translate and logic handlers

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-var transform = require('./lib/transform.js');
-var mask = require('./lib/mask.js');
-var slice = require('./lib/slice.js');
+var transform = require('./lib/transform');
+var translate= require('./lib/translate');
+var mask = require('./lib/mask');
+var slice = require('./lib/slicer');
+var logic = require('./lib/logic');
 
 exports.transform = transform;
+exports.translate = translate;
 exports.mask = mask;
 exports.slice = slice;
+exports.logic = logic;
