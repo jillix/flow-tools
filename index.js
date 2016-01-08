9e5bc05aa611c47a44518719bbc12c4a0500dd71
Adrian Ottiker <adrian@ottiker.com>
2016-01-08 10:21:53 +0000

    export slice and mask methods

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 var transform = require('./lib/transform.js');
+var mask = require('./lib/mask.js');
+var slice = require('./lib/slice.js');
 
 exports.transform = transform;
+exports.mask = mask;
+exports.slice = slice;
