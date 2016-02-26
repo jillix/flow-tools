ce55cb3071f226f751a6a96eeaa9f17ae8672a35
Adrian Ottiker <adrian@ottiker.com>
2016-02-26 14:28:02 +0000

    export all methods

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-var transform = require('./lib/transform');
+var libob = require('libobject');
+var change = require('./lib/change');
 var translate = require('./lib/translate');
 var mask = require('./lib/mask');
 var slice = require('./lib/slicer');
-var logic = require('./lib/logic');
+var join = require('./lib/join');
+var dom = require('./lib/dom');
+var url = require('./lib/url');
+//var logic = require('./lib/logic');
 
-exports.transform = transform;
+exports.deep = function (options, data, next) {
+
+    data = libob.deep(data);
+
+    if (data instanceof Error) {
+        return next(data);
+    }
+
+    next(null, data);
+};
+
+exports.flat = function (options, data, next) {
+
+    data = libob.flat(data);
+
+    if (data instanceof Error) {
+        return next(data);
+    }
+
+    next(null, data);
+};
+
+exports.change = change;
 exports.translate = translate;
 exports.mask = mask;
 exports.slice = slice;
-exports.logic = logic;
+exports.join = join;
+exports.url = url;
+exports.dom = dom;
+//exports.logic = logic;
