591452a8a5a29286e5d6b5707ce21138d3e2aac1
Adrian Ottiker <adrian@ottiker.com>
2016-03-02 09:31:01 +0000

    export "logic" handler

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var libob = require('libobject');
 var transform = require('./lib/transform');
 var translate = require('./lib/translate');
+var logic = require('./lib/logic');
 //var mask = require('./lib/mask');
 //var slice = require('./lib/slicer');
 //var join = require('./lib/join');
-//var logic = require('./lib/logic');
 
 exports.deep = function (options, data, next) {
 
@@ -30,7 +30,7 @@ exports.flat = function (options, data, next) {
 
 exports.transform = transform;
 exports.translate = translate;
+exports.logic = logic;
 //exports.mask = mask;
 //exports.slice = slice;
 //exports.join = join;
-//exports.logic = logic;
