b8fb7a3ed86e1adf68a3741119dc5fa02c99f12b
Dan Andrei <andreidani96@gmail.com>
2016-04-21 12:04:05 +0300

    Added slice method.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var translate = require('./lib/translate');
 var logic = require('./lib/logic');
 var mask = require('./lib/mask');
 var json = require('./lib/json');
-//var slice = require('./lib/slicer');
+var slice = require('./lib/slicer');
 //var join = require('./lib/join');
 
 exports.deep = function (options, data, next) {
@@ -35,5 +35,5 @@ exports.transform = transform;
 exports.translate = translate;
 exports.logic = logic;
 exports.mask = mask;
-//exports.slice = slice;
+exports.slice = slice;
 //exports.join = join;
